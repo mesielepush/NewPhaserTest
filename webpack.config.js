e1fd0382563dcb940ec03a30e52bb7151e55ba8e
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-07-21 18:43:36 -0500

    deploying

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,10 @@ module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
   devtool: 'eval-source-map',
-
+  output:{
+    filename: 'main.js',
+    path: path.resolve(__dirname,'../dist'),
+  },
   module: {
     rules: [
       {
